1d9ffbf7b7f6a5d3985072e7e2fe2e7143913e5d
Akin Evans <akinevans@gmail.com>
2023-06-21 14:27:48 -0700

    Build Carousel  scroll functionality

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Carousel.css";
 import c1 from "../../assets/images/home/carousel-1/carousel_101.webp";
 import c2 from "../../assets/images/home/carousel-1/carousel_102.webp";
@@ -12,10 +12,20 @@ import c8 from "../../assets/images/home/carousel-1/carousel_108.jpg";
 //! Images should scroll on X axis with both scroll wheels
 
 export default function Carousel(props) {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
   const landingData1 = [c1, c2, c3, c4, c5, c6, c7, c8];
   //   variable for each carousels list of images
   //   variable for each carousels list of images
   //   variable for each carousels list of images
+
+  const prevSlide = () => {
+    setCurrentSlide(currentSlide === 0 ? 7 : (prev) => prev - 1);
+  };
+
+  const nextSlide = () => {
+    setCurrentSlide(currentSlide === 7 ? 0 : (prev) => prev - 1);
+  };
 
   function getImages(data) {
     for (let i = 0; i < data.length; i++) {
@@ -24,18 +34,23 @@ export default function Carousel(props) {
   }
   return (
     <div className={`carousel-outer-wrapper ${props.className}`}>
-      <div className={`carousel-inner-wrapper ${props.className}`}>
+      <div
+        className={`carousel-inner-wrapper ${props.className}`}
+        style={{ transform: `translateX(-${currentSlide * 100}vw)` }}
+      >
         {/* add utility classes for each carousel size */}
         {/* write a function to get image data instead of hard coding it */}
         {/* {getImages(landingData1)} */}
-        <img className={props.className} src={landingData1[0]} alt='' />
-        <img className={props.className} src={landingData1[1]} alt='' />
-        <img className={props.className} src={landingData1[2]} alt='' />
-        <img className={props.className} src={landingData1[3]} alt='' />
-        <img className={props.className} src={landingData1[4]} alt='' />
-        <img className={props.className} src={landingData1[5]} alt='' />
-        <img className={props.className} src={landingData1[6]} alt='' />
-        <img className={props.className} src={landingData1[7]} alt='' />
+        <div className='carousel-img-wrapper'>
+          <img className={props.className} src={landingData1[0]} alt='' />
+          <img className={props.className} src={landingData1[1]} alt='' />
+          <img className={props.className} src={landingData1[2]} alt='' />
+          <img className={props.className} src={landingData1[3]} alt='' />
+          <img className={props.className} src={landingData1[4]} alt='' />
+          <img className={props.className} src={landingData1[5]} alt='' />
+          <img className={props.className} src={landingData1[6]} alt='' />
+          <img className={props.className} src={landingData1[7]} alt='' />
+        </div>
       </div>
     </div>
   );
