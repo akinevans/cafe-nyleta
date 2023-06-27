ff0ec6b72f1327689076a86481caad57bd41631d
Akin Evans <akinevans@gmail.com>
2023-06-27 09:48:33 -0700

    Refactor Hero component to accept props to make it reusable on multiple pages

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,26 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Hero.css";
-import hero_img from "../../assets/images/home/feature.jpg";
 import ClientsSection from "../ClientsSection/ClientsSection";
 import Button from "../Button/Button";
 
-export default function Hero() {
+export default function Hero(props) {
   return (
     <section className='hero-outer-wrapper'>
       <div className='hero-inner-wrapper'>
         <div className='hero-text-wrapper'>
-          <p>
-            Cafe Nyleta is a Los Angeles-based fashion consultancy with a team
-            of expert designers providing end-to-end support. We specialize in
-            producing high-quality garments, sourcing premium materials, and
-            developing product lines that capture your brand's vision.
-          </p>
+          <p>{props.message}</p>
         </div>
 
         <img
           className='hero-img'
-          src={hero_img}
+          src={props.image}
           alt='clothing model standing in the woods'
         />
 
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -5,20 +5,31 @@ import ServicesComp from "../components/ServicesComp/ServicesComp";
 import Quote from "../components/Quote/Quote";
 import Factories from "../components/Factories/Factories";
 
+//image imports
+import landing_img from "../../src/assets/images/home/feature.jpg";
+
 export default function Landing(props) {
-  const quoteData = {
-    text: "“Design is creativity, expertise, and collaboration. It involves attention to detail and a commitment to bringing ideas to life in a way that reflects the brand's vision and values”",
-    author: "Jack Mahaley, Lead Designer",
-  };
+  const data = [
+    {
+      landingHeroText:
+        "Cafe Nyleta is a Los Angeles-based fashion consultancy with a team of expert designers providing end-to-end support. We specialize in producing high-quality garments, sourcing premium materials, and developing product lines that capture your brands vision.",
+    },
+    {
+      landingQuoteText:
+        "“Design is creativity, expertise, and collaboration. It involves attention to detail and a commitment to bringing ideas to life in a way that reflects the brand's vision and values”",
+      landingAuthor: "Jack Mahaley, Lead Designer",
+    },
+  ];
   return (
     <>
-      <Hero />
+      {/* Navbar and Footer are rendered in index.js */}
+      <Hero message={data[0].landingHeroText} image={landing_img} />
       <Carousel page='landing1' />
       <ServicesComp />
       <Quote
         //
-        quoteText={quoteData.text}
-        author={quoteData.author}
+        quoteText={data[1].landingQuoteText}
+        author={data[1].landingAuthor}
       />
       <Carousel page='landing2' />
       <Factories />
