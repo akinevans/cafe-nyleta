7d8447fd44f7fa1cb213e19959071c62ad14f6fe
Akin Evans <akinevans@gmail.com>
2023-07-07 16:03:52 -0700

    update form submit

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -9,7 +9,7 @@ export default function ContactForm() {
     <div className='contact-form-outer-wrapper'>
       <form
         className='contact-form'
-        action='https://formsubmit.co/akinevans@gmail.com'
+        action='https://formsubmit.co/dc734c4144a39d0767d0d689469a0ea1'
         method='POST'
       >
         <h1 className='form-title'>Contact</h1>
