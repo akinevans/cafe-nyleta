33173af365c660a1d7ba62caf46ddddfbc8f7e31
Akin Evans <akinevans@gmail.com>
2023-07-26 09:49:12 -0700

    Cafe Nyleta logo in footer now behaves as a link. Taking the user back to the top of the homepage

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,11 +4,22 @@ import "./Footer.css";
 import logo from "../../assets/icons/cafe-nyleta-logo.png";
 
 export default function Footer() {
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  };
+
   return (
     <footer className='footer-outer-wrapper'>
       <div className='footer-content-wrapper'>
-        <img src={logo} alt='' />
-        {/* Replace with router Link elements */}
+        <Link
+          //
+          to='/'
+          onClick={() => {
+            scrollToTop();
+          }}
+        >
+          <img src={logo} alt='cafe nyleta logo' />
+        </Link>
         <Link to='/privacy-policy'>Privacy Policy</Link>
         <Link to='/terms-and-conditions'>Terms & Conditions</Link>
         <Link to='/careers'>Careers</Link>
