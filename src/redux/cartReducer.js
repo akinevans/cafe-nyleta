541343be986aaddac8d0d90817750dac95b36a0f
Akin Evans <akinevans@gmail.com>
2023-12-06 13:52:36 -0800

    Complete refactoring of addToCart function. Fix all current bugs

--- src/redux/cartReducer.js
+++ src/redux/cartReducer.js
@@ -15,7 +15,12 @@ export const cartSlice = createSlice({
 
       if (item) {
         if (state.products.includes(item)) {
-          //!loop until a match is found, if no match at end of array, push to cart
+          // mutate from shoppingCartItem
+          if (action.payload.newQuantity) {
+            item.itemQuantity = action.payload.newQuantity;
+            return;
+          }
+          //loop until a match is found, if no match found by end of array, push to cart
           for (let i = 0; i < state.products.length; i++) {
             let isIdMatch = state.products[i].id === action.payload.id;
             let isSizeMatch = state.products[i].size === action.payload.size;
@@ -28,37 +33,29 @@ export const cartSlice = createSlice({
                   state.products[i].itemQuantity + action.payload.itemQuantity >
                   maxQuantity
                 ) {
-                  alert("Limit 5 per customer");
-
+                  alert("Limit 5 per customer, per size");
                   state.products[i].itemQuantity = maxQuantity;
+                  break;
                   //TODO: build custom alert modal
                 } else {
                   state.products[i].itemQuantity += action.payload.itemQuantity;
+                  break;
                 }
               }
-
-              // mutate from shoppingCartItem
-              if (action.payload.newQuantity) {
-                state.products[i].itemQuantity = action.payload.newQuantity;
-              }
             } else {
-              // are we at the end of the state array?
-              //if yes, that means no match was found, push item into array
+              // check if at end of the state array
+              //if true, that means no match was found, push item into array
               if (i === state.products.length - 1) {
-                console.log("NO match found " + isIdMatch + " " + isSizeMatch);
+                // console.log("NO match found " + isIdMatch + " " + isSizeMatch);
                 alert("failed third check, no matches found");
-                //! dont immediately push item into state,
                 state.products.push(action.payload);
               } else {
-                //continue looping
                 continue;
               }
             }
           } // end  loop
         } else {
           alert("failed second check");
-          //! dont immediately push item into state,
-
           state.products.push(action.payload);
         }
       } else {
