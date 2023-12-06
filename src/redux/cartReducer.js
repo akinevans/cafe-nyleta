94524b3873a618099ab0aaad9f24acfe815627d9
Akin Evans <akinevans@gmail.com>
2023-12-06 13:24:05 -0800

    Continue refactoring addToCart function

--- src/redux/cartReducer.js
+++ src/redux/cartReducer.js
@@ -11,39 +11,54 @@ export const cartSlice = createSlice({
     addToCart: (state, action) => {
       // search for item in state cart array, find by id
       let item = state.products.find((item) => item.id === action.payload.id);
-      // now that we have the product id, filter by size
-      item = state.products.find((item) => item.size === action.payload.size);
       const maxQuantity = 5;
 
-      //TODO: iterate over state, calculate item totals for each product by size, if total > maxQuantity throw error
-
-      //* check if item exists in state array and check if this item exists in array in its current payload size  */
       if (item) {
-        if (
-          state.products.includes(item) &&
-          action.payload.size === item.size
-        ) {
-          //!check if this item, in the payload size is already in the cart
-          if (action.payload.itemQuantity) {
-            // mutate from waitlistModal / addToCart component
-            if (item.itemQuantity + action.payload.itemQuantity > maxQuantity) {
-              item.itemQuantity = maxQuantity;
-              //TODO: build custom alert modal
-              alert("Limit 5 per customer");
-            } else {
-              item.itemQuantity += action.payload.itemQuantity;
-            }
-          }
+        if (state.products.includes(item)) {
+          //!loop until a match is found, if no match at end of array, push to cart
+          for (let i = 0; i < state.products.length; i++) {
+            let isIdMatch = state.products[i].id === action.payload.id;
+            let isSizeMatch = state.products[i].size === action.payload.size;
 
-          // mutate from shoppingCartItem
-          if (action.payload.newQuantity) {
-            item.itemQuantity = action.payload.newQuantity;
-          }
+            if (isIdMatch && isSizeMatch) {
+              console.log("Match found " + isIdMatch + " " + isSizeMatch);
+              if (action.payload.itemQuantity) {
+                // mutate from waitlistModal aka addToCart component
+                if (
+                  state.products[i].itemQuantity + action.payload.itemQuantity >
+                  maxQuantity
+                ) {
+                  alert("Limit 5 per customer");
+
+                  state.products[i].itemQuantity = maxQuantity;
+                  //TODO: build custom alert modal
+                } else {
+                  state.products[i].itemQuantity += action.payload.itemQuantity;
+                }
+              }
+
+              // mutate from shoppingCartItem
+              if (action.payload.newQuantity) {
+                state.products[i].itemQuantity = action.payload.newQuantity;
+              }
+            } else {
+              // are we at the end of the state array?
+              //if yes, that means no match was found, push item into array
+              if (i === state.products.length - 1) {
+                console.log("NO match found " + isIdMatch + " " + isSizeMatch);
+                alert("failed third check, no matches found");
+                //! dont immediately push item into state,
+                state.products.push(action.payload);
+              } else {
+                //continue looping
+                continue;
+              }
+            }
+          } // end  loop
         } else {
           alert("failed second check");
-
           //! dont immediately push item into state,
-          // add item to state products array
+
           state.products.push(action.payload);
         }
       } else {
