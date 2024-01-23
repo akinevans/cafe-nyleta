19399a539690a0f6c2cb2fb68051d898057380c6
Akin Evans <akinevans@gmail.com>
2024-01-23 13:25:31 -0800

    Update cart modal total price text. Remove top and bottom borders from ShoppingCartItem. Add top and bottom border to the wrapper that holds all of the shoppingCartItem components.

--- src/components/ShoppingCartModal/ShoppingCartModal.js
+++ src/components/ShoppingCartModal/ShoppingCartModal.js
@@ -40,26 +40,30 @@ export default function ShoppingCartModal(props) {
         >
           Cart Empty
         </h1>
-        {products.map((data) => (
-          <ShoppingCartItem
-            id={data.id}
-            //! update key once sizes are included, multiple children will have the same id (id + size will work)
-            key={data.id + data.size}
-            name={data.title}
-            price={data.price}
-            size={data.size}
-            individualItemQuantity={data.itemQuantity}
-            totalItemPrice={data.itemQuantity * data.price}
-            imgSrc={process.env.REACT_APP_UPLOAD_URL + data?.image}
-            imgAlt={data.title}
-            navigationLink={`/itemdetail/${data.id}`}
-            closeBtnOnClick={props.closeBtnOnClick}
-          />
-        ))}
+        <div className='shopping-cart-item-component-wrapper'>
+          {products.map((data) => (
+            <ShoppingCartItem
+              id={data.id}
+              //! update key once sizes are included, multiple children will have the same id (id + size will work)
+              key={data.id + data.size}
+              name={data.title}
+              price={data.price}
+              size={data.size}
+              individualItemQuantity={data.itemQuantity}
+              totalItemPrice={data.itemQuantity * data.price}
+              imgSrc={process.env.REACT_APP_UPLOAD_URL + data?.image}
+              imgAlt={data.title}
+              navigationLink={`/itemdetail/${data.id}`}
+              closeBtnOnClick={props.closeBtnOnClick}
+            />
+          ))}
+        </div>
         <div className='total-wrapper'>
           <h1 className={`cart-text ${cartEmpty ? "hidden" : ""}`}>
             {`Total (${getCartQuantityAndPrice()[1]}):`}{" "}
-            <span className='cart-price'>${getCartQuantityAndPrice()[0]}</span>{" "}
+            <span className='cart-price'>
+              ${getCartQuantityAndPrice()[0]} USD
+            </span>{" "}
           </h1>
         </div>
         <div className='cart-btn-wrapper'>
